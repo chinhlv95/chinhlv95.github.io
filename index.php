17c53a661b3b7ff60a153962edaf890a090e6aad
chinhlv95 <chinhlv_58@vnu.edu.vn>
2018-05-21 18:26:27 +0700

    fdfdfd

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Get File Template</title>
+	<link rel="stylesheet" type="text/css" href="public/css/style.css">
+</head>
+<body>
+	<div class="form-style">
+		<h2>GENERATE SCS STG UPDATE MANUAL</h2>
+		    <form method="POST" id="form" action="./mvc/view/index.php">
+			    <input type="text" name="total" placeholder="TicketID/Git branch (Ex: SCS-3170)" required />
+			    <input type="submit" value="GENERATE">
+		    </form>
+	</div>
+</body>
+</html>
